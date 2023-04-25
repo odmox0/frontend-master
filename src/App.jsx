85fb7adc5ff476041f3e361ee6064d2ca84ce867
Oscar Moreno <oscar.david.moreno@gmail.com>
2023-04-25 14:50:08 -0700

    oaut implementation finish

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,25 @@
 
+import { useEffect } from 'react';
 import Home from './pages/home/home';
 import Profile from './pages/profile/profile';
 import { Route, Routes } from 'react-router';
+import { useAuth0  } from '@auth0/auth0-react'; 
+
 import './App.css'
 
-
 const App= () => {
+  
+  const {isAuthenticated, isLoading, loginWithRedirect } = useAuth0();
+  
+  useEffect(() => {
+    if(!isAuthenticated && !isLoading){
+      loginWithRedirect();
+    }
+  },[isAuthenticated, isLoading])
+  
+  if(isLoading){return <h1>Loading...</h1>}
+  
+
   return (
     <Routes>
       <Route path="/" element={<Home/>}/>
