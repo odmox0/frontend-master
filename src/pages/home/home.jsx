556e84b042c12678593219db681cb75a3b70b62e
Oscar Moreno <oscar.david.moreno@gmail.com>
2023-04-25 21:45:19 -0700

    addGoogleMaps

--- src/pages/home/home.jsx
+++ src/pages/home/home.jsx
@@ -1,9 +1,20 @@
+import { useMemo } from "react"
+import { GoogleMap, useLoadScript, Marker } from "@react-google-maps/api"
 import General from "../../layout/general/general"
+import "./home.css"
 
 const Home = () => {
+    const { isLoaded } = useLoadScript({googleMapsApiKey : import.meta.env.VITE_GOOGLE_MAPS_API_KEY })    
+    const center = useMemo(() => ({lat: -34.397, lng: 150.644}), [])
     return (
         <General>
-            Welcome
+            {!isLoaded ? <h1>Loading...</h1> : <GoogleMap 
+            zoom={10}
+            center={center}
+            mapContainerClassName="map-container"
+            >
+                <Marker position={center}/>
+            </GoogleMap>}
         </General>
     )
 }
